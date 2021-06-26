9cff9c81e0abd8d01dd3eb0e060357b6b64e7530
xenhim <33386927+xenhim@users.noreply.github.com>
2021-06-26 04:08:10 -0400

    Update ethash.go

--- ethash.go
+++ ethash.go
@@ -41,6 +41,7 @@ import (
 	"unsafe"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
 )
